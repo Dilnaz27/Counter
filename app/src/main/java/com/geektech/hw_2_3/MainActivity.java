0ac38c3d2411f20dcea531d68a9e4cda20b5c2ac
Dilnaz27 <dbaudunova@gmail.com>
2023-01-13 23:43:27 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/hw_2_3/MainActivity.java
@@ -0,0 +1,15 @@
+package com.geektech.hw_2_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
